6f209b3edbf7c653d79affae6edd6f4509b1ae8a
Saman Mokhtari <saman.mokhtari99@gmail.com>
2025-10-04 12:34:40 +0330

    Feat/financial/mobile (#1)
    * Some moidifications
    
    * refactor(IP): Host IP changed.
    
    * feat(Transaction): Transaction page created for mobile.

--- src/components/StatsCard.jsx
+++ src/components/StatsCard.jsx
@@ -2,7 +2,7 @@ export default function StatsCard({ children, title, className }) {
     return (
         <div
             className={`${className} w-full desktop:w-[18rem] p-4 border border-dashed rounded-xl border-textPrimary flex flex-col gap-3`}>
-            <h2 className="text-[18px] font-bold">{title}</h2>
+            <h2 className="text-[18px] font-semibold">{title}</h2>
             <div className="text-[20px] font-light">{children}</div>
         </div>
     )
